4d5229c4c86d45cbf16e6dfa11df776088d98158
Gabriel Lacerda <devlacerda1109@gmail.com>
2022-02-16 22:05:27 -0300

    Adiciona interface do body

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-    return <div className="App">
-        <header>
-            <div className="headerContent">
-                <p className="appLogo"><span>$</span>ToDoTerminal</p>
-                <input
-                    type="text"
-                    placeholder="Insira seu nome"
-                />
-            </div>
-        </header>
-    </div>;
+    return (
+        <div className="App">
+            <header>
+                <div className="headerContent">
+                    <p className="appLogo">
+                        <span>$</span>ToDoTerminal
+                    </p>
+                    <input type="text" placeholder="Insira seu nome" />
+                </div>
+            </header>
+            <main>
+                <div className="terminal">
+                    <div className="commandLine">
+                        <p>
+                            Gabriel: <span>~$</span>
+                        </p>
+                        <input type="text" spellCheck="false" />
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
